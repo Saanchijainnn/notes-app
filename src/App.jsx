84703380e2c5458a62ecfd8ffc26cb36a1c96e59
saanchijainnn <saanchi2315@gmail.com>
2026-06-16 23:57:12 +0530

    final structure

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ e.preventDefault();
 
   return (
     <div className="h-screen lg:flex  bg-black text-white ">
-      <h1 className="text-3xl font-bold">Add Notes</h1>
+     
       <form onSubmit={(e)=>{
         submitHandler(e);
       } } className="flex  items-start lg:w-1/2 flex-col gap-4 p-10">
+         <h1 className="text-4xl font-bold">Add Notes</h1>
         
         <input
          type="text"
@@ -27,8 +28,8 @@ e.preventDefault();
         <button className="bg-white  w-full text-black px-5 py-2 font-medium outline-none rounded">Add Note</button>
         
       </form>
-      <div className=" lg:w-1/2 border-l-2 p-10">
-      <h1 className="text-3xl font-bold"> Your Notes </h1>
+      <div className=" lg:w-1/2 lg:border-l-2 p-10">
+      <h1 className="text-4xl font-bold"> Recent Notes </h1>
         <div className="flex gap-5 flex-wrap mt-5 h-full overflow-auto">
         <div className="h-52 w-40 rounded-2xl bg-white "></div>
         <div className="h-52 w-40 rounded-2xl bg-white "></div>
